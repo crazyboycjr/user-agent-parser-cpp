dabb9a9dfde46777c5f344bc35b4fc5063acb504
crazyboycjr <crazyboycjr@gmail.com>
2016-08-18 11:47:05 +0800

    update the output format

--- src/bootstrap.cpp
+++ src/bootstrap.cpp
@@ -6,16 +6,18 @@ void doTest() {
 	const int MAXN = 10000;
 	char teststr[MAXN];
 
-	int cnt = 0;
 	UAConfig::ConfigLoader config("regexes/oses.yml", "regexes/browsers.yml", "regexes/mobiles.yml");
 	while (fgets(teststr, MAXN, stdin)) {
 		teststr[strlen(teststr) - 1] = '\0';
 		UAParser::Parser parser(&config, teststr);
+		parser.getOS();
+		parser.getBrowser();
+		parser.getMobile();
 
-		printf("%d\n", ++cnt);
-		printf("OSname:%s\t%s\t", parser.getOS().name.c_str(), parser.getOS().version.c_str());
-		printf("Client:%s\t%s\t", parser.getBrowser().name.c_str(), parser.getBrowser().version.c_str());
-		printf("Device:%s\t%s\t%s\n", parser.getMobile().brand.c_str(), parser.getMobile().type.c_str(), parser.getMobile().model.c_str());
+		printf("1|%s|%s|%s|", parser.getMobile().brand.c_str(), parser.getMobile().type.c_str(), parser.getMobile().model.c_str());
+		printf("%s|%s|", parser.getBrowser().version.c_str(), parser.getBrowser().name.c_str());
+		printf("%s|%s|", parser.getOS().version.c_str(), parser.getOS().name.c_str());
+		printf("%s\n", parser.getText().c_str());
 //		printf("OSname:%s\t%s\tClient:%s\t%s\tDevice:%s\t%s\t%s\n", parser.getOS().name.c_str(), parser.getOS().version.c_str(),
 //				parser.getBrowser().name.c_str(), parser.getBrowser().version.c_str(),
 //				parser.getMobile().brand.c_str(), parser.getMobile().type.c_str(), parser.getMobile().model.c_str());
