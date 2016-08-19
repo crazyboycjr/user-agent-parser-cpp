f4e3b2b8c2abc1cc57ec025b52d38aa90853c1be
crazyboycjr <crazyboycjr@gmail.com>
2016-08-19 12:08:06 +0800

    增加了杂牌的处理

--- src/bootstrap.cpp
+++ src/bootstrap.cpp
@@ -6,7 +6,7 @@ void doTest() {
 	const int MAXN = 10000;
 	char teststr[MAXN];
 
-	UAConfig::ConfigLoader config("regexes/oses.yml", "regexes/browsers.yml", "regexes/mobiles.yml");
+	UAConfig::ConfigLoader config("/home/cjr/Developing/user-agent-parser-cpp/regexes/oses.yml", "/home/cjr/Developing/user-agent-parser-cpp/regexes/browsers.yml", "/home/cjr/Developing/user-agent-parser-cpp/regexes/mobiles.yml");
 	while (fgets(teststr, MAXN, stdin)) {
 		teststr[strlen(teststr) - 1] = '\0';
 		UAParser::Parser parser(&config, teststr);
@@ -14,9 +14,9 @@ void doTest() {
 		parser.getBrowser();
 		parser.getMobile();
 
-		printf("1|%s|%s|%s|", parser.getMobile().brand.c_str(), parser.getMobile().type.c_str(), parser.getMobile().model.c_str());
-		printf("%s|%s|", parser.getBrowser().version.c_str(), parser.getBrowser().name.c_str());
-		printf("%s|%s|", parser.getOS().version.c_str(), parser.getOS().name.c_str());
+		printf("1|||%s|||%s|||%s|||", parser.getMobile().brand.c_str(), parser.getMobile().type.c_str(), parser.getMobile().model.c_str());
+		printf("%s|||%s|||", parser.getBrowser().version.c_str(), parser.getBrowser().name.c_str());
+		printf("%s|||%s|||", parser.getOS().version.c_str(), parser.getOS().name.c_str());
 		printf("%s\n", parser.getText().c_str());
 //		printf("OSname:%s\t%s\tClient:%s\t%s\tDevice:%s\t%s\t%s\n", parser.getOS().name.c_str(), parser.getOS().version.c_str(),
 //				parser.getBrowser().name.c_str(), parser.getBrowser().version.c_str(),
